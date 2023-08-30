7d6ef5682b1e7268536e160927bcfd801b6faf25
laluRiascos <110619768+laluRiascos@users.noreply.github.com>
2023-08-29 23:54:51 -0500

    Componente_PuntoC

---
+++ src/Comp3.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+const Comp3 = () => {
+
+    const [hora, setHora] = useState()
+    const [nombre, setNombre] = useState()
+    const [saludo, setSaludo] = useState()
+
+    const onSetHora = ( evt ) => {
+        setHora( evt.target.value ) 
+    }
+
+    const onSetNombre = ( evt ) => {
+        setNombre( evt.target.value ) 
+    }
+
+    const BrindarSaludo = () =>
+    {
+        var horaH = parseInt(hora);
+
+        if (horaH < 12) {
+            setSaludo(`Buenos Dias, ${nombre}`);
+        }if (12 < horaH && horaH < 19){
+            setSaludo(`Buenos Tardes, ${nombre}`);
+        }if (horaH > 19){
+            setSaludo(`Buenos Noches, ${nombre}`);
+        }
+    }
+
+    return(
+        <>
+        <h1> Ejercicio #03 </h1>
+        <h2> Saludo </h2>
+        <p> Diligencia tu nombre: </p>
+        <input type="text" value={nombre}
+        onChange={(event) => onSetNombre(event)}/>
+        <br/>
+        <br/>
+        <p> Diligencia la hora en formato 24h </p>
+        <input type="text" value={hora}
+        onChange={(event) => onSetHora(event)}/>
+        <br/>
+        <button onClick={() => BrindarSaludo()}> Saludo </button>
+        <p>{saludo}</p>
+        </>
+    )
+}
+
+export default Comp3
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Comp1 from './Comp1.jsx'
 import Comp2 from './Comp2.jsx'
+import Comp3 from './Comp3.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Comp1/>
     <Comp2/>
+    <Comp3/>
   </React.StrictMode>,
 )
